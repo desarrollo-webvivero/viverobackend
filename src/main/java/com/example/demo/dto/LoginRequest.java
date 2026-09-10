54096b2600d3c06009e7b6059cf62c3d80193eaa
JoseSaquiche <josetzay0@gmail.com>
2026-09-10 13:58:35 -0600

    fix: Guardar DTOs e importaciones de stock

---
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto;
+
+public class LoginRequest {
+    private String correoElectronico;
+    private String contrasena;
+
+    public String getCorreoElectronico() { return correoElectronico; }
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContrasena() { return contrasena; }
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
